4593f16b50f59f63d315989be4e2406a803a5b4a
Johnny Tirado <johnny.tirado.ii@gmail.com>
2022-01-14 18:31:08 -0500

    Removing previous branding of the bot.
    This bot was forked from a branded project and should remain
    unbranded for future ones.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-A simple bot written in Python3 (https://www.python.org/) that uses the PRAW module (https://praw.readthedocs.io/en/stable/index.html). Responds to certain key phrases from comments submitted in the r/AnarchyChess subreddit, and spams lyrics from a highly amusing video created by IM (International Chess Master) Levy Rozman on his Youtube channel, as a reply, with love. (https://youtu.be/7MRNWxCuIrI)
+A simple bot written in Python 3 (https://www.python.org/) that uses the PRAW module (https://praw.readthedocs.io/en/stable/index.html) to communicate with Reddit. Responds to certain key phrases from comments (post titles and comments planned for a future update) with a comment reply.
 
-Inspired by u/FMHall's bot, u/PetrosianBot. That bot brought a lot of joy to the subreddit, so I figured why not add to the fun? Code was not derived from that Redditor's bot. The two are developed separately.
+This was originally created as a reply bot for a subreddit, but was banned there. The project however was a success, so I want to continue refining this project to be as useful as possible.
 
-This bot is obviously open sourced and will be licensed under GPL v2. Feel free to fork and tweak to your heart's content as long as you abide by the licensing rules.
+This bot is open sourced and will be licensed under GPL v2. Feel free to fork and tweak to your heart's content as long as you abide by the licensing rules.
--- replybot5k.py
+++ replybot5k.py
@@ -3,7 +3,7 @@
 # Modules
 import praw, datetime, configparser
 
-# WIP: Read config file for credentials.
+# Read config file for credentials.
 config = configparser.ConfigParser()
 config.read('login.ini')
 
@@ -14,7 +14,7 @@ username = config['login']['username']
 password = config['login']['password']
 user_agent = config['login']['user_agent']
   
-# Creating an authorized reddit instance.
+# Creating an authorized Reddit instance.
 reddit = praw.Reddit(client_id = client_id, client_secret = client_secret, username = username, password = password, user_agent = user_agent) 
   
 # The subreddit to monitor.
@@ -25,9 +25,9 @@ subreddit = reddit.subreddit(target_sub)
 print("Monitoring " + target_sub + "...")
   
 # Phrases that trigger the bot.
-trigger_list = ['1660', 'on move 6', 'trapped queen', 'trapped the man\'s queen', 'trap the man\'s queen', 'like a potato']
+trigger_list = ['foo', 'bar']
   
-# Check every comment in the subreddit except mine.
+# Check every comment in the subreddit except the bot's.
 me = reddit.user.me()
 for comment in subreddit.stream.comments(skip_existing=True):
   
@@ -35,14 +35,7 @@ for comment in subreddit.stream.comments(skip_existing=True):
     if [ele for ele in trigger_list if(ele in comment.body.lower())] and comment.author != me:
   
         # initialize the reply text
-        reply_text = """1660\. 1660. You're telling me a 1600 hung a piece on move 6? **YOU ARE TELLING ME** THAT A _1600 RATED PLAYER HUNG A PIECE ON MOVE 6, 1600 RATED PLAYER HUNG A PIECE ON MOVE 6, THIS GUY IS 1660 AND HE HUNG A PIECE ON MOVE 6; 16, 16, 60 and he hung a piece on move 6!_
-
-_Didn't see a trapped queen, could've trapped the man's queen. Didn't trap the man's queen, could've trapped the man's queen, this man had Rook a4 like 3 moves in a row, didn't even see it._
-Like when I see the queen come here this is the first thing I think of.
-
-_Damn. Damn. Damn damn damn. Well at least this dude has potato in his name because he_... played like a potato. He played like a potato.
-
-^Beep boop. | [Source](https://github.com/jclocks/levy-rozbot) | [Issues? Suggestions?](https://github.com/jclocks/levy-rozbot/issues) | [Video](https://www.youtube.com/watch?v=7MRNWxCuIrI)"""
+        reply_text = ''
           
         # Shitpost.
         comment.reply(reply_text)
