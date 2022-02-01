a8449ac3530eabba1ded35172ca6408deb74bae4
Johnny Tirado <johnny.tirado.ii@gmail.com>
2022-01-31 21:24:06 -0500

    Issue #1: Handle Reddit API crashes via loop

--- replybot5k.py
+++ replybot5k.py
@@ -5,44 +5,52 @@ import praw
 import datetime
 import configparser
 
-# Read config file for credentials.
-config = configparser.ConfigParser()
-config.read('login.ini')
+# Error handling loop
+while True:
+    try:
 
-# Prompt for credentials.
-client_id = config['login']['client_id']
-client_secret = config['login']['client_secret']
-username = config['login']['username']
-password = config['login']['password']
-user_agent = config['login']['user_agent']
+        # Read config file for credentials.
+        config = configparser.ConfigParser()
+        config.read('login.ini')
 
-# Creating an authorized Reddit instance.
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                     username=username, password=password,
-                     user_agent=user_agent)
+        # Prompt for credentials.
+        client_id = config['login']['client_id']
+        client_secret = config['login']['client_secret']
+        username = config['login']['username']
+        password = config['login']['password']
+        user_agent = config['login']['user_agent']
 
-# The subreddit to monitor.
-target_sub = "testingground4bots"
-subreddit = reddit.subreddit(target_sub)
+        # Creating an authorized Reddit instance.
+        reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                             username=username, password=password,
+                             user_agent=user_agent)
 
-# Let the user know you're up.
-print("Monitoring " + target_sub + "...")
+        # The subreddit to monitor.
+        target_sub = "testingground4bots"
+        subreddit = reddit.subreddit(target_sub)
 
-# Phrases that trigger the bot.
-trigger_list = ['foo', 'bar']
+        # Let the user know you're up.
+        print("Monitoring " + target_sub + "...")
 
-# Check every comment in the subreddit except the bot's.
-me = reddit.user.me()
-for comment in subreddit.stream.comments(skip_existing=True):
+        # Phrases that trigger the bot.
+        trigger_list = ['foo', 'bar']
 
-    # Check the trigger_phrase in each comment
-    if ([ele for ele in trigger_list if(ele in comment.body.lower())]
-            and comment.author != me):
+        # Check every comment in the subreddit except the bot's.
+        me = reddit.user.me()
+        for comment in subreddit.stream.comments(skip_existing=True):
 
-        # initialize the reply text
-        reply_text = ''
+            # Check the trigger_phrase in each comment
+            if [ele for ele in trigger_list if(ele in comment.body.lower())]
+            and comment.author != me:
 
-        # Shitpost.
-        comment.reply(reply_text)
-        print(str(datetime.datetime.now()) + ": Replied to comment "
-              + str(comment.id) + " by " + str(comment.author))
+                # initialize the reply text
+                reply_text = 'Reply text goes here.'
+
+                # Perform comment and log to stdout.
+                comment.reply(reply_text)
+                print(str(datetime.datetime.now()) + ": Replied to comment "
+                      + str(comment.id) + " by " + str(comment.author))
+
+    except prawcore.exceptions.ResponseException:
+        time.sleep(10)
+        continue
